abc1b50d81c0a980467001c7a8ee7ae116ee538e
T-Welsh <you@example.comsalthook@gmail.com>
2023-06-30 10:46:00 +0100

    basic game implementation

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Player {
         this.position = position; // {x, y}
         this.velocity = velocity;
         this.rotation = rotation;
+        this.hullIntegrity = 100;
         }
 
     draw() {
@@ -37,6 +38,36 @@ class Player {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
     }
+
+    handleImpact() {
+        this.hullIntegrity -=1;
+        if (this.hullIntegrity <= 0) {
+            console.log(`SHIP LOST`)
+            return true;
+        }
+        console.log(`HULL INTEGRITY: ${this.hullIntegrity}`);
+        return false;
+    }
+
+    getVertices() {
+        const cos = Math.cos(this.rotation)
+        const sin = Math.sin(this.rotation)
+    
+        return [
+          {
+            x: this.position.x + cos * 30 - sin * 0,
+            y: this.position.y + sin * 30 + cos * 0,
+          },
+          {
+            x: this.position.x + cos * -10 - sin * 10,
+            y: this.position.y + sin * -10 + cos * 10,
+          },
+          {
+            x: this.position.x + cos * -10 - sin * -10,
+            y: this.position.y + sin * -10 + cos * -10,
+          },
+        ]
+      }
 }
 
 class Projectile {
@@ -68,6 +99,11 @@ class Asteroid {
         this.radius = 50 * Math.random() + 10;
     }
 
+    damage() {
+        this.radius -= 10;
+        this.radius <= 0 ? console.log(`Asteroid Destroyed`) :console.log(`Remaining Asteroid Integrity: ${this.radius}`);
+    }
+
     draw() {
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
@@ -83,8 +119,7 @@ class Asteroid {
     }
 }
 
-
-
+// GAME VARIABLES
 const player = new Player({
         position: {x:(canvas.width / 2), y:(canvas.height / 2)},
         velocity: {x:0, y:0},
@@ -120,7 +155,7 @@ const PROJECTILE_THRUST = 3;
 const projectiles = [];
 const asteroids = [];
 
-window.setInterval( () => {
+const asteroidSpawning = window.setInterval( () => {
 
     const asteroidSpawnIndex = Math.floor(Math.random() * 5);
 
@@ -179,13 +214,75 @@ window.setInterval( () => {
     }
 }, 2000);
 
+
+//fUNCTIONS
+function circleCollision(circle1, circle2) {
+    const xDifference = circle2.position.x - circle1.position.x;
+    const yDifference = circle2.position.y - circle1.position.y;
+    const distanceBetweenCenters = Math.sqrt((xDifference * xDifference) + (yDifference * yDifference));
+
+    if (circle1.radius + circle2.radius >= distanceBetweenCenters) {
+        console.log(`Hit Detected`);
+        return true;
+    }
+
+    return false;
+}
+
+function circleTriangleCollision(circle, triangle) {
+    // Check if the circle is colliding with any of the triangle's edges
+    for (let i = 0; i < 3; i++) {
+      let start = triangle[i]
+      let end = triangle[(i + 1) % 3]
+  
+      let dx = end.x - start.x
+      let dy = end.y - start.y
+      let length = Math.sqrt(dx * dx + dy * dy)
+  
+      let dot =
+        ((circle.position.x - start.x) * dx +
+          (circle.position.y - start.y) * dy) /
+        Math.pow(length, 2)
+  
+      let closestX = start.x + dot * dx
+      let closestY = start.y + dot * dy
+  
+      if (!isPointOnLineSegment(closestX, closestY, start, end)) {
+        closestX = closestX < start.x ? start.x : end.x
+        closestY = closestY < start.y ? start.y : end.y
+      }
+  
+      dx = closestX - circle.position.x
+      dy = closestY - circle.position.y
+  
+      let distance = Math.sqrt(dx * dx + dy * dy)
+  
+      if (distance <= circle.radius) {
+        return true
+      }
+    }
+  
+    // No collision
+    return false
+  }
+  
+  function isPointOnLineSegment(x, y, start, end) {
+    return (
+      x >= Math.min(start.x, end.x) &&
+      x <= Math.max(start.x, end.x) &&
+      y >= Math.min(start.y, end.y) &&
+      y <= Math.max(start.y, end.y)
+    )
+  }
+
 function animate() {
-    window.requestAnimationFrame(animate);
+    const animationId = window.requestAnimationFrame(animate);
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,canvas.width,canvas.height);
     player.update();
 
-    //loop through projectiles array from back to front. this stops flashing.
+        //PROJECTILE MANAGMENT
+    //loop through projectiles array from back to front. this stops flashing.as
     for(let i = projectiles.length - 1; i >= 0; i--) {
         const projectile = projectiles[i];
         projectile.update();
@@ -199,17 +296,39 @@ function animate() {
         }
     }
 
+    //ASTEROID MANAGMENT
     //loop through asteroids array from back to front. this stops flashing.
     for(let i = asteroids.length - 1; i >= 0; i--) {
         const asteroid = asteroids[i];
         asteroid.update();
         
+        if (circleTriangleCollision(asteroid, player.getVertices())) {
+            const playerDestroyed = player.handleImpact();
+            if (playerDestroyed) {
+                window.cancelAnimationFrame(animationId);
+                clearInterval(asteroidSpawning);
+            }
+        }
+
         //Clean up asteroids off screen
         if( asteroid.position.x + asteroid.radius < - 60 ||
             asteroid.position.x - asteroid.radius > canvas.width + 60 ||
             asteroid.position.y - asteroid.radius > canvas.height + 60||
             asteroid.position.y + asteroid.radius < -60) {
             asteroids.splice(i, 1);
+        }
+
+        //Collision Checking
+        for(let j = projectiles.length - 1; j >= 0; j--) {
+            const projectile = projectiles[j];
+            if (circleCollision(projectile ,asteroid)) {
+                asteroid.damage();
+                if (asteroid.radius <= 0) {
+                    asteroids.splice(i, 1);
+                }
+                
+                projectiles.splice(j, 1);
+            }
         }
     }
 
@@ -233,24 +352,23 @@ function animate() {
     }
 }
 
+
+//EVENT LISTENERS
 window.addEventListener('keydown', (event) => {
     switch (event.code) {
         case 'KeyW':
             keys.w.pressed = true;
-            console.log(player.velocity.x);
             break;
         case 'KeyA':
             keys.a.pressed = true;
             break;
         case 'KeyS':
             keys.s.pressed = true;
-            console.log(player.velocity.x);
             break;
         case 'KeyD':
             keys.d.pressed = true;
             break;
         case 'Space':
-            console.log(asteroids);
             projectiles.push(new Projectile({
                 position: {
                     x: player.position.x + Math.cos(player.rotation) * 30, // offset by player edge
@@ -287,4 +405,5 @@ window.addEventListener('keyup', (event) => {
     }
 });
 
+//INIT GAME LOOP
 animate();
